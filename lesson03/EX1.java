e273e3f22b729d55e25d565e9e5f4c1aa7cfbfbc
Dungbv <dungbv2201@gmail.com>
2023-04-27 20:09:34 +0700

    ex1

--- lesson03/EX1.java
+++ lesson03/EX1.java
@@ -8,17 +8,20 @@ public class EX1 {
         System.out.println("Enter: ");
         int param = sc.nextInt();
         int max =0;
+        int count=0;
         while (param != 0) {
             int number = param % 10;
-            if(number == 9){
-                max = number;
-                break;
-            }
             if(max < number){
                 max = number;
+                count =1;
+            }
+            if(max == number){
+                count++;
             }
             param /= 10;
         }
-        System.out.printf("So lon nhat: %d", max);
+        int rs = count -1;
+        System.out.printf("So lon nhat: %d \n", max);
+        System.out.printf("countt: %d", rs);
     }
 }
