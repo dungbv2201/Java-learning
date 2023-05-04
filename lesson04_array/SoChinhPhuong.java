0f134b2331e57623176f606d3ad5b0b3845cb913
Dungbv <dungbv2201@gmail.com>
2023-05-04 21:15:03 +0700

    so chinh phuong

---
+++ lesson04_array/SoChinhPhuong.java
@@ -0,0 +1,27 @@
+package lesson04_array;
+
+import java.util.Scanner;
+
+public class SoChinhPhuong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N: ");
+        int length = sc.nextInt();
+        int [] arr = new int[length];
+        for(int i = 0; i< length; i++){
+            System.out.printf("Arr[%d]=: ", i);
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i< length; i++){
+            if(Math.sqrt(arr[i]) % 1 == 0){
+                count++;
+            }
+        }
+        if(count == 0){
+            System.out.print("khong co so chinh phuong");
+        }else{
+            System.out.printf("count %d", count);
+        }
+    }
+}
